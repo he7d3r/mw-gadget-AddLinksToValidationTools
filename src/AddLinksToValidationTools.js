4f52ae9376ca0bb851a485032c04dbf37f730d48
Helder <he7d3r@gmail.com>
2014-09-27 20:48:55 -0300

    Apply MediaWiki Coding Conventions
    Validated with jscs 1.6.2, using preset "wikimedia"

--- src/AddLinksToValidationTools.js
+++ src/AddLinksToValidationTools.js
@@ -7,47 +7,47 @@
  * License: Creative Commons Attribution-ShareAlike (BY-SA) 3.0
  */
 ( function ( mw, $ ) {
-'use strict';
+	'use strict';
 
-function addLinkToValidateCSS () {
-	var validator = 'http://jigsaw.w3.org/css-validator/validator?&usermedium=all&warning=1&lang=en&uri=',
-		uri = 'http:' + mw.config.get( 'wgServer' ) + mw.config.get( 'wgScript' ) + '?' +
-			$.param( {
-				title: mw.config.get( 'wgPageName' ),
-				oldid: mw.config.get( 'wgCurRevisionId' ),
-				action: 'raw',
-				ctype: 'text/css'
-			} );
-	mw.util.addPortletLink(
-		'p-cactions',
-		validator + encodeURIComponent( uri ),
-		'Validate CSS',
-		'ca-validate-css',
-		'Validate CSS',
-		'v',
-		null
-	);
-}
+	function addLinkToValidateCSS() {
+		var validator = 'http://jigsaw.w3.org/css-validator/validator?&usermedium=all&warning=1&lang=en&uri=',
+			uri = 'http:' + mw.config.get( 'wgServer' ) + mw.config.get( 'wgScript' ) + '?' +
+				$.param( {
+					title: mw.config.get( 'wgPageName' ),
+					oldid: mw.config.get( 'wgCurRevisionId' ),
+					action: 'raw',
+					ctype: 'text/css'
+				} );
+		mw.util.addPortletLink(
+			'p-cactions',
+			validator + encodeURIComponent( uri ),
+			'Validate CSS',
+			'ca-validate-css',
+			'Validate CSS',
+			'v',
+			null
+		);
+	}
 
-function addLinkToValidateHTML () {
-	var validator = 'http://validator.w3.org/check?uri=',
-		uri = 'http:' + mw.config.get( 'wgServer' ) + mw.config.get( 'wgScript' ) +
-			'?oldid=' + mw.config.get( 'wgCurRevisionId' );
-	mw.util.addPortletLink(
-		'p-cactions',
-		validator + encodeURIComponent( uri ),
-		'Validate HTML',
-		'ca-validate-html',
-		'Validate HTML',
-		'v',
-		null
-	);
-}
+	function addLinkToValidateHTML() {
+		var validator = 'http://validator.w3.org/check?uri=',
+			uri = 'http:' + mw.config.get( 'wgServer' ) + mw.config.get( 'wgScript' ) +
+				'?oldid=' + mw.config.get( 'wgCurRevisionId' );
+		mw.util.addPortletLink(
+			'p-cactions',
+			validator + encodeURIComponent( uri ),
+			'Validate HTML',
+			'ca-validate-html',
+			'Validate HTML',
+			'v',
+			null
+		);
+	}
 
-if ( /\.css$/i.test( mw.config.get( 'wgTitle' ) ) && $.inArray( mw.config.get( 'wgNamespaceNumber' ), [ 2, 8 ] ) !== -1 ) {
-	$( addLinkToValidateCSS );
-} else {
-	$( addLinkToValidateHTML );
-}
+	if ( /\.css$/i.test( mw.config.get( 'wgTitle' ) ) && $.inArray( mw.config.get( 'wgNamespaceNumber' ), [ 2, 8 ] ) !== -1 ) {
+		$( addLinkToValidateCSS );
+	} else {
+		$( addLinkToValidateHTML );
+	}
 
 }( mediaWiki, jQuery ) );
